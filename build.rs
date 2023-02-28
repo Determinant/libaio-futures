38637ec37f9318593ff934e2a0e2552675d96a55
Ron Kuris <ron.kuris@avalabs.org>
2023-02-28 15:38:31 -0800

    Add build dependency checks
    Unless you install gcc and make, you get really obscure errors.
    This happens because we don't save or render the output from the build.
    
    This diff adds some sanity checks and panics with a sensible error.

--- build.rs
+++ build.rs
@@ -1,15 +1,28 @@
 use std::env;
+use std::ffi::OsStr;
+use std::path::PathBuf;
+use std::io::{self, Write};
 
 fn main() {
+    exe_exists("make").expect("Can't find make; perhaps install with: apt install make");
+    exe_exists("cc").expect("Can't find cc: perhaps install with: apt install gcc");
     let out_dir = env::var("OUT_DIR").unwrap();
-    std::process::Command::new("make")
+    let output = std::process::Command::new("make")
         .args(&[format!("{}/libaio.a", out_dir)])
         .env("OUT_DIR", &out_dir)
         .current_dir("./libaio")
-        .status()
-        .expect("failed to make libaio");
-    eprintln!("{}", out_dir);
+        .output()
+        .expect("make failed");
+    if !output.status.success() {
+        io::stderr().write_all(&output.stderr).unwrap();
+        panic!("make failed");
+    }
     // the current source version of libaio is 0.3.112
-    println!("cargo:rerun-if-changed={}/libaio.a", out_dir);
+    println!("cargo:rerun-if-changed=libaio");
     println!("cargo:rustc-link-search=native={}", out_dir);
+}
+
+fn exe_exists(exe: &str) -> Option<PathBuf> {
+    let path = std::env::var_os("PATH").unwrap_or_else(|| OsStr::new("").into());
+    std::env::split_paths(&path).find(|path| path.join(exe).exists())
 }
